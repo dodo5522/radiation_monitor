e9b22c41e7b1a59d8dd67b3439553e38496b53d7
Takashi Ando <dodo5522@gmail.com>
2017-02-27 07:32:48 +0900

    まずは雛形を追加

--- test/test_main.py
+++ test/test_main.py
@@ -1,5 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
+import unittest
+from unittest.mock import MagicMock, patch
+from radiation_monitor.__main__ import main_routine
+
+
+class TestMainRoutine(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    @patch("radiation_monitor.argparser")
+    @patch("radiation_monitor.config")
+    @patch("radiation_monitor.logger")
+    @patch("radiation_monitor.source.GeigerMeter")
+    def test_exceptions(self, mock_geiger_meter_, mock_logger_, mock_config_, mock_argparser_):
+        print(mock_geiger_meter_)
+        print(mock_logger_)
+        print(mock_config_)
+        print(mock_argparser_)
+        main_routine()
+
+
 if __name__ == "__main__":
-    pass
+    unittest.main()
